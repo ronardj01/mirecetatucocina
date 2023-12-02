f90eed3df0ee14939660b82bb37e4e7f107231d2
ronardj01 <ronardj01@gmail.com>
2023-12-02 09:51:18 +0100

    enviar información desde dashboard.php hasta eliminar.php por la url

--- administracion/dashboard/eliminar.php
+++ administracion/dashboard/eliminar.php
@@ -15,17 +15,45 @@
 </head>
 
 <body>
-<?php 
-session_start();
-require_once("../includes/functions.php");
-secure();
-$active_eliminar = true;
-include_once("../includes/header.php");
-?>
-  
+  <?php
+  session_start();
+  require_once("../includes/functions.php");
+  secure();
+  $active_eliminar = true;
+  include_once("../includes/header.php");
+  if (isset($_GET['rec']) && isset($_GET['nombre']) && isset($_GET['imagen'])) {
+    $id_receta = $_GET['rec'];
+    $nombre = $_GET['nombre'];
+    $imagen = $_GET['imagen'];
+  }
+  ?>
+
   <main class="container mt-5">
-  <?php include_once("../includes/titulo.php") ?>
-</main>
+    <?php include_once("../includes/titulo.php") ?>
+
+    <div class="row row-cols-1 row-cols-md-5">
+      <div class='col'>
+        <div class="card">
+          <img src="../../img/<?php echo $imagen ?>" class="card-img-top" alt="<?php echo $nombre ?>">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title text-center pt-4">
+            <?php echo $nombre ?>
+          </h5>
+          <p class="card-text text-center py-2">Estas a punto de elminar esta receta permanentemente</p>
+        </div>
+      </div>
+      <div class="col col-md-5 col-lg-4">
+        <form action="#" method="post" autocomplete="off">
+          <div class="mb-3">
+            <label for="nombreReceta" class="form-label">Escriba el nombre de la receta</label>
+            <input type="text" class="form-control fs-5" id="nombreReceta" name="nombreReceta" autocomplete="off">
+          </div>
+          <button type="submit" class="btn btn-primary">Iniciar Sesión</button>
+        </form>
+      </div>
+    </div>
+  </main>
 </body>
 
 </html>
