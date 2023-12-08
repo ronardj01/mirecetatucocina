77ff7b53d6ec85bb2253bb0be98d240c19981807
ronardj01 <ronardj01@gmail.com>
2023-12-08 13:29:26 +0100

    agregar tynyCME

--- administracion/dashboard/crear.php
+++ administracion/dashboard/crear.php
@@ -13,7 +13,19 @@ require_once("../includes/head.php");
 
   <main class="container mt-5">
     <?php include_once("../includes/titulo.php") ?>
+    <form action="#" method="POST" autocomplete="off">
+    <textarea name="preparacion" id="preparacion">Hello, World!</textarea>
+  </form>
   </main>
+ 
+
+  <!-- Codigo de tinyMCE -->
+  <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+  <script>
+    tinymce.init({
+      selector: '#preparacion'
+    });
+  </script>
 </body>
 
 </html>
