ed286b6e1c970972de8a2abd23cafa6055ec2c9f
ronardj01 <ronardj01@gmail.com>
2023-12-09 07:22:39 +0100

    crear formulario para llenar tabla recetas

---
+++ administracion/includes/categoriasFieldset.php
@@ -0,0 +1,46 @@
+<fieldset class="border border-4 p-3 mb-3">
+  <legend class="fs-3 col-5 text-center mb-3 fw-bold">Categorias</legend>
+  <div>
+    <div class="form-check form-check-inline fs-5">
+      <input class="form-check-input" type="checkbox" name="carnes" id="carnes" value="1">
+      <label class="form-check-label" for="carnes">Carnes</label>
+    </div>
+    <div class="form-check form-check-inline fs-5">
+      <input class="form-check-input" type="checkbox" name="pescados" id="pescados" value="2">
+      <label class="form-check-label" for="pescados">Pescados</label>
+    </div>
+    <div class="form-check form-check-inline fs-5">
+      <input class="form-check-input" type="checkbox" name="arroz" id="arroz" value="3">
+      <label class="form-check-label" for="arroz">Arroz y Pastas</label>
+    </div>
+    <div class="form-check form-check-inline fs-5">
+      <input class="form-check-input" type="checkbox" name="ensaladas" id="ensaladas" value="4">
+      <label class="form-check-label" for="ensaladas">Ensaladas</label>
+    </div>
+    <div class="form-check form-check-inline fs-5">
+      <input class="form-check-input" type="checkbox" name="sopas" id="sopas" value="5">
+      <label class="form-check-label" for="sopas">Sopas y Cremas</label>
+    </div>
+    <div class="mt-2">
+      <div class="form-check form-check-inline fs-5">
+        <input class="form-check-input" type="checkbox" name="salsas" id="salsas" value="6">
+        <label class="form-check-label" for="salsas">Salsas</label>
+      </div>
+      <div class="form-check form-check-inline fs-5">
+        <input class="form-check-input" type="checkbox" name="postres" id="postres" value="7">
+        <label class="form-check-label" for="postres">Postres</label>
+      </div>
+      <div class="form-check form-check-inline fs-5">
+        <input class="form-check-input" type="checkbox" name="panes" id="panes" value="8">
+        <label class="form-check-label" for="arroz">Panes y Pizzas</label>
+      </div>
+      <div class="form-check form-check-inline fs-5">
+        <input class="form-check-input" type="checkbox" name="aperitivos" id="aperitivos" value="9">
+        <label class="form-check-label" for="aperitivos">Aperitivos</label>
+      </div>
+      <div class="form-check form-check-inline fs-5">
+        <input class="form-check-input" type="checkbox" name="bebidas" id="bebidas" value="10">
+        <label class="form-check-label" for="bebidas">Cócteles y Bebidas</label>
+      </div>
+    </div>
+</fieldset>
---
+++ administracion/includes/mediaFieldset.php
@@ -0,0 +1,19 @@
+<fieldset class="border border-4 p-3 mb-3">
+  <legend class="fs-3 col-5 mb-3 fw-bold">Multimedia</legend>
+  <div class="col-10 fs-4 mb-3">
+    <label for="video" class="form-label">Video</label>
+    <textarea name="video" id="video" class="form-control" rows="4"></textarea>
+  </div>
+  <div class="mb-3 col-10">
+    <label for="imagen1" class="form-label fs-4">Imagen Principal</label>
+    <input type="text" class="form-control form-control-lg" id="imagen1" name="imagen1">
+  </div>
+  <div class="mb-3 col-10">
+    <label for="imagen2" class="form-label fs-4">Imagen Opcional</label>
+    <input type="text" class="form-control form-control-lg" id="imagen2" name="imagen2">
+  </div>
+  <div class="mb-3 col-10">
+    <label for="imagen3" class="form-label fs-4">Imagen Opcional</label>
+    <input type="text" class="form-control form-control-lg" id="imagen3" name="imagen3">
+  </div>
+</fieldset>
