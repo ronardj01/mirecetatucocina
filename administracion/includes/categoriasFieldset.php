606653391cc670b1baccda092d6e03998b023d04
ronardj01 <ronardj01@gmail.com>
2023-12-28 09:32:04 +0100

    crear subcategorias

--- administracion/includes/categoriasFieldset.php
+++ administracion/includes/categoriasFieldset.php
@@ -43,4 +43,55 @@
         <label class="form-check-label" for="bebidas">Cócteles y Bebidas</label>
       </div>
     </div>
+    <?php //Sub Categorias?>
+    <fieldset class="p-3 mb-3">
+      <legend class="fs-4 col-5 mb-3 mt-3 fw-bold">Sub-Categorias</legend>
+      <div>
+        <div class="form-check form-check-inline fs-5">
+          <input class="form-check-input" type="checkbox" name="pollo" id="pollo" value="1">
+          <label class="form-check-label" for="pollo">Pollo</label>
+        </div>
+        <div class="form-check form-check-inline fs-5">
+          <input class="form-check-input" type="checkbox" name="ternera" id="ternera" value="2">
+          <label class="form-check-label" for="ternera">Ternera</label>
+        </div>
+        <div class="form-check form-check-inline fs-5">
+          <input class="form-check-input" type="checkbox" name="cerdo" id="cerdo" value="3">
+          <label class="form-check-label" for="cerdo">Cerdo</label>
+        </div>
+        <div class="form-check form-check-inline fs-5">
+          <input class="form-check-input" type="checkbox" name="cordero" id="cordero" value="4">
+          <label class="form-check-label" for="cordero">Cordero</label>
+        </div>
+        <div class="form-check form-check-inline fs-5">
+          <input class="form-check-input" type="checkbox" name="cabra" id="cabara" value="5">
+          <label class="form-check-label" for="cabra">Cabra</label>
+        </div>
+        <div class="form-check form-check-inline fs-5">
+          <input class="form-check-input" type="checkbox" name="pavo" id="pavo" value="6">
+          <label class="form-check-label" for="pavo">Pavo</label>
+        </div>
+        <div class="form-check form-check-inline fs-5">
+          <input class="form-check-input" type="checkbox" name="pato" id="pato" value="7">
+          <label class="form-check-label" for="pato">Pato</label>
+        </div>
+        <div class="form-check form-check-inline fs-5">
+          <input class="form-check-input" type="checkbox" name="guinea" id="guinea" value="8">
+          <label class="form-check-label" for="guinea">Guinea</label>
+        </div>
+        <div class="form-check form-check-inline fs-5">
+          <input class="form-check-input" type="checkbox" name="conejo" id="conejo" value="9">
+          <label class="form-check-label" for="conejo">Conejo</label>
+        </div>
+        <div class="form-check form-check-inline fs-5">
+          <input class="form-check-input" type="checkbox" name="fria" id="fria" value="10">
+          <label class="form-check-label" for="fria">Fria</label>
+        </div>
+        <div class="form-check form-check-inline fs-5">
+          <input class="form-check-input" type="checkbox" name="caliente" id="caliente" value="11">
+          <label class="form-check-label" for="caliente">Caliente</label>
+        </div>
+      </div>
+
+    </fieldset>
 </fieldset>
